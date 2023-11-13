ae7d23c8a0aec3439a6e8e50dd1f666c6070fb30
HuseynovaDilara <huseynova.8885@gmail.com>
2023-11-13 06:41:54 +0400

    Task added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_12._11._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    interface IAcccount
+    {
+        public string PasswordChecker(string password);
+        public bool ShowInfo
+        {
+
+        }
+
+    }
+    class User : IAcccount
+    {
+        public string Fullame { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
